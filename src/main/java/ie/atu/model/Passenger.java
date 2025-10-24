df5ebf5305e95676a052c2d8cb200f28a87c424d
Alvin Olayemi <g00428825@atu.ie>
2025-10-24 19:19:40 +0100

    Passenger Class passengerID,name,email

--- src/main/java/ie/atu/model/Passenger.java
+++ src/main/java/ie/atu/model/Passenger.java
@@ -1,7 +1,24 @@
 package ie.atu.model;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor @Builder
 public class Passenger
 {
+ @NotBlank @Size(max = 40 )
+    private String PassengerID;
 
+ @NotBlank @Size (max = 60)
+    private String name;
+
+ @NotBlank @Email
+    private String email;
 }
